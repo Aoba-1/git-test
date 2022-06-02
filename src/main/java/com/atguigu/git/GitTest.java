9fc998d6071b14ecca7d0d75b6825300849f2f72
Blue <1792041821@qq.com>
2022-06-02 23:54:40 +0800

    master commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,13 +3,9 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
         System.out.println("master test");
         System.out.println("hot-fix");
         System.out.println("master import");
+
     }
 }
